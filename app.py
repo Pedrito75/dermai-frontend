bb566d9c3fe2123943c3ea4cec8520d96cbded47
Pierre BOLOT <bolotpierre@hotmail.fr>
2025-11-16 23:43:41 +0100

    first push of the DermAI frontend. Next step: separate the backend on an API running on Cloud Run

---
+++ app.py
@@ -0,0 +1,107 @@
+import requests
+import pandas as pd
+from PIL import Image
+import streamlit as st
+
+from params import *
+from api_simulator import MODEL_DATA, MODELS, predict_mole
+
+if "models" not in st.session_state:
+    st.session_state.models = MODELS
+
+if "model_labels" not in st.session_state:
+    st.session_state.model_labels = [m["label"] for m in MODEL_DATA.values()]
+
+if "is_uploaded" not in st.session_state:
+    st.session_state.is_uploaded = False
+
+if "image" not in st.session_state:
+    st.session_state.image = None
+
+# window tile setup
+st.set_page_config(page_title="DermAI", page_icon="🔍", layout="centered")
+
+# add css styling
+with open('./styles.css') as f:
+    css = f.read()
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
+st.header("DermAI Assistant", divider="grey")
+
+st.markdown("## Choose your model")
+
+selected_model_label = st.selectbox(
+    "Which asistant do you want for your prediction?",
+    st.session_state.model_labels,
+)
+
+with st.container(border=True):
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown("## Upload a mole")
+
+        mode = st.radio(
+            "How do you want to provide a photo?",
+            ("Upload a file", "Use camera"),
+            horizontal=True,
+        )
+
+        if mode == "Upload a file":
+
+            if not st.session_state.is_uploaded:
+                # Show uploader only if no image selected yet
+                file = st.file_uploader(
+                    "Upload an image",
+                    type=["jpg", "jpeg", "png"],
+                    key="uploader"
+                )
+                if file:
+                    st.session_state.is_uploaded = True
+                    st.session_state.image = file
+                    st.rerun()
+
+            else:
+                st.image(st.session_state.image, caption="Uploaded image")
+                if st.button("Choose another image"):
+                    st.session_state.is_uploaded = False
+                    st.rerun()
+
+        else:
+            st.session_state.is_uploaded = False
+            picture = st.camera_input("Take a photo")
+            if picture:
+                st.session_state.image = picture
+
+    with col2:
+        with st.container(horizontal=True, vertical_alignment="bottom"):
+            st.markdown("## Results")
+
+            predict_btn = st.button("Predict", type="primary")
+
+        with st.container(height="stretch", vertical_alignment="bottom"):
+            if predict_btn:
+                if not st.session_state.image:
+                    st.warning("You need add an image!")
+                else:
+                    with st.spinner(text="Predicting your fate...", show_time=True):
+                        image = Image.open(st.session_state.image)
+                        model = st.session_state.models[st.session_state.model_labels.index(selected_model_label)]
+
+                        pred = pd.DataFrame(predict_mole(image, model)).T
+                        pred.index = pred.index.map(CODE_TO_CLASS)
+                        pred.columns = ["Probabilities"]
+                        pred = pred.sort_values(by="Probabilities", ascending=False)
+                        pred["color"] = pred.index.map(CLASS_TO_COLOR)
+                        pred["hexa"] = pred["color"].map(COLOR_TO_HEXA)
+
+                        with st.container(height="stretch", vertical_alignment="top"):
+                            best_pred = pred.iloc[0]
+                            class_pred = CLASS_TO_NAME[best_pred.name]
+                            if best_pred["color"] == "green":
+                                st.success(f"You should be fine 👌\nI bet it's a `{class_pred}`")
+                            elif best_pred["color"] == "yellow":
+                                st.warning(f"Hum this is weird 🤔\nI recommend you check what could be a `{class_pred}` when you have time")
+                            else:
+                                st.error(f"It looks like a `{class_pred}` ⚠️\nDon't wait to see a professional")
+
+                        st.bar_chart(data=pred, y="Probabilities", color="hexa", horizontal=True, sort="-Probabilities")
---
+++ params.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# ----- api host -----
+SERVICE_URL = os.getenv("SERVICE_URL")
+# --------------------
+
+# ----- dataset classes -----
+CODE_TO_CLASS = {0: "akiec", 1: "bcc", 2: "bkl", 3: "df", 4: "mel", 5: "nv", 6: "vasc"}
+CLASS_TO_NAME = {
+    "akiec": "Actinic keratoses and intraepithelial carcinoma / Bowen's disease",
+    "bcc": "Basal cell carcinoma",
+    "bkl": "Benign keratosis-like lesions",
+    "df": "Dermatofibroma",
+    "mel": "Melanoma",
+    "nv": "Melanocytic nevi",
+    "vasc": "Vascular lesions",
+}
+COLOR_TO_HEXA = {
+    "green": "#7EB42E",
+    "yellow": "#EEFF34",
+    "red": "#FF6F61",
+}
+CLASS_TO_COLOR = {
+    "akiec": "yellow",
+    "bcc": "red",
+    "bkl": "green",
+    "df": "green",
+    "mel": "red",
+    "nv": "green",
+    "vasc": "green",
+}
+# ---------------------------
